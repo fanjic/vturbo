0aed4c8bd3271640332c872639cff46a6a10124a
fanjic <404844679@qq.com>
2023-12-04 03:16:48 +0800

    new

--- src/test/java/com/fan/vturbo/PersonTest.java
+++ src/test/java/com/fan/vturbo/PersonTest.java
@@ -12,8 +12,6 @@ import org.elasticsearch.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 
 import javax.annotation.Resource;
 import java.io.IOException;
